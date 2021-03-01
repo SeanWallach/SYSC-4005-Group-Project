4e20d4cd49c927d3a4860237041d7301c6373032
Sean Wallach <wallachsean1@gmail.com>
2021-02-28 22:31:12 -0500

    updated Workstation notation
    updated Workstation notation

--- src/SimModel.java
+++ src/SimModel.java
@@ -6,7 +6,7 @@ public class SimModel {
     // Simulation Model Variables
     
     private static Queue<SimEvent> FEL;                                             // This is the FEL
-    public static Queue<workstation> W1C1Q, W2C1Q, W2C2Q, W3C1Q, W3C3Q;             // Queue lines for the buffer unit
+    public static Queue<Workstation> W1C1Q, W2C1Q, W2C2Q, W3C1Q, W3C3Q;             // Queue lines for the buffer unit
     private static boolean isW1C1QBusy, isW2C1QBusy, isW2C2QBusy, isW3C1QBusy, isW3C3QBusy;                            
 
     // Inputs
@@ -23,9 +23,9 @@ public class SimModel {
        
 
         // Creating workstations
-        workstation W1 = new workstation(1);
-        workstation W2 = new workstation(2);
-        workstation W3 = new workstation(3);
+        Workstation W1 = new Workstation(1);
+        Workstation W2 = new Workstation(2);
+        Workstation W3 = new Workstation(3);
        
 
         // set up any other initial settings here ;)
