be88400e4f869f0a213e4f0ba05f922b7b938ac1
Sean Wallach <wallachsean1@gmail.com>
2021-02-28 22:34:53 -0500

    Update SimEvent.java

--- src/SimEvent.java
+++ src/SimEvent.java
@@ -1,5 +1,6 @@
 public class SimEvent {
-  public enum eventType {}
+  // AI - arrive at Inspector    EI - end inspect    AW - arrive at workstation   DW - depart workstation
+  public enum eventType {AI,EI,AW,DW}
   
   public SimEvent () {
   } 
