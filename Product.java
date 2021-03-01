1da9b53a95e0680ab361b588e037883ef3f2efb3
Sean Wallach <wallachsean1@gmail.com>
2021-02-28 21:33:39 -0500

    Create Product.java
    Product superclass initial commit

---
+++ Product.java
@@ -0,0 +1,29 @@
+// Superclass of all products
+public class Product {
+  
+  public enum serviceType {OFF,LOADER1,LOADER2,TRAVEL,WAITING};
+  
+  public Product(int ID) {
+    this.ID = ID
+    this.whichService = serviceType.OFF;
+  }
+  
+  private int ID;
+  private serviceType whichService;
+  
+  public int getID() {
+        return ID;
+  }
+
+  public void setID(int ID) {
+      this.ID = ID;
+  }
+
+  public serviceType getWhichService() {
+      return whichService;
+  }
+
+  public void setWhichService(serviceType whichService) {
+      this.whichService = whichService;
+  }
+}
