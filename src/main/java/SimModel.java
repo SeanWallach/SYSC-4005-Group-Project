65ac78bc9f2ba162c2fd056735c85f1531962d37
LAPTOP-ASUS\Sean <wallachsean1@gmail.com>
2021-04-01 17:34:13 -0400

    More scheduling/processing functionality

--- src/main/java/SimModel.java
+++ src/main/java/SimModel.java
@@ -5,10 +5,12 @@ public class SimModel {
 
     // Simulation Model Variables
     public static int Clock;
+    public static Random RNGInspectionComponent1, RNGInspectionComponent2, RNGInspectionComponent3,RNGWorkstation1,RNGWorkstation2,RNGWorkstation3;      // Variables for random numbers generated for each event
     private static Queue<SimEvent> FEL;                                             // This is the FEL
-    public static Queue<Component> W1C1Q, W2C1Q, W2C2Q, W3C1Q, W3C3Q,I1,I2;             // Queue lines for the buffer unit
+    public static Queue<Component> W1C1Q, W2C1Q, W2C2Q, W3C1Q, W3C3Q,I1,I2;         // Queue lines for the buffer unit
     private static boolean isW1C1QBusy, isW2C1QBusy, isW2C2QBusy, isW3C1QBusy, isW3C3QBusy, isW1BUsy, isW2Busy, isW3Busy;
     private static boolean isI1Busy, isI2Busy;
+
     // Inputs
     public static double[][] servinp1 = {{5,0.35},{15,0.78},{25,0.93},{50,0.99},{80,1}};
     public static double[][] servinp2 = {{5,0.22},{15,0.61},{25,0.81},{50,0.7},{115,1}};
@@ -35,7 +37,16 @@ public class SimModel {
     }
 
     private static void Initialization() {
-        FEL = new PriorityQueue<SimEvent>();            // Initializing the FEL and waiting queues
+        // RNG initialization
+        RNGInspectionComponent1 = new Random();
+        RNGInspectionComponent2 = new Random();
+        RNGInspectionComponent3 = new Random();
+        RNGWorkstation1 = new Random();
+        RNGWorkstation2 = new Random();
+        RNGWorkstation3 = new Random();
+
+        //Buffer initialization
+        FEL = new PriorityQueue<SimEvent>();            // Initializing the FEL
         W1C1Q = new LinkedList<>();
         W2C1Q = new LinkedList<>();
         W2C2Q = new LinkedList<>();
@@ -62,6 +73,7 @@ public class SimModel {
         System.out.print("\n-----------------------------------------------------------\n");
         System.out.print("Initial state of the simulation\n");
         System.out.print("Component1 at Inspector1, but no components in the buffers yet.\n");
+        System.out.print("Component2 at Inspector2, but no components in the buffers yet.\n");
 
         Component1.setWhichService(Component.serviceType.INSPECTOR1);
         Component2.setWhichService(Component.serviceType.INSPECTOR2);
@@ -103,25 +115,31 @@ public class SimModel {
 
     }
 
-    private void ProcessEI(SimEvent evt){
+    private static void ProcessEI(SimEvent evt){
         System.out.print(" event = Component " + evt.getComponent().getID() + "leaves Inspector" + evt.getInspector().getID());
         //if ()
+    }
+
+    private static void ProcessEW(SimEvent imminentEVT) {
+    }
+
+    private static void ProcessAW(SimEvent imminentEVT) {
     }
 
     // Schedules events with their components
     private static void ScheduleEvent(SimEvent.eventType type, Component component) {
         Integer newRN = -1;
         switch (type) {
-            case AI:
-//                newRN = getRandomTime(TTD,RNGtravel);   // These need to be changed
-//                break;
-//            case EI:
-//                newRN = getRandomTime(LTD,RNGloading); // These need to be changed
-//                break;
-//            case AW:
-//                newRN = getRandomTime(WTD,RNGscale);     // These need to be changed
-//                break;
-            case EW:
+            case AI: // Arrive at Inspector
+                newRN = getRandomTime(servinp1,RNGInspectionComponent1);   // These need to be changed
+                break;
+            case EI: // End Inspection
+              //  newRN = getRandomTime(LTD,RNGloading); // These need to be changed
+                break;
+            case AW: // Arrive at Workstation
+              //  newRN = getRandomTime(WTD,RNGscale);     // These need to be changed
+                break;
+            case EW: // End Workstation
                 //newRN = getRandomTime();
         }
         checkSimDay(newRN);
@@ -130,6 +148,26 @@ public class SimModel {
         FEL.offer(newEVT);
     }
 
+    // Processes events
+    private static void processSimEvent(SimEvent imminentEVT) {
+        System.out.print("Processing events...");
+        switch (imminentEVT.geteType()) {
+            case AI:
+                ProcessAI(imminentEVT);
+                break;
+            case EI:
+                ProcessEI(imminentEVT);
+                break;
+            case AW:
+                ProcessAW(imminentEVT);
+                break;
+            case EW:
+                ProcessEW(imminentEVT);
+        }
+        System.out.print("\n");
+    }
+
+    // Not implemented yet
     private static void checkSimDay(Integer newRN) {
     }
 
